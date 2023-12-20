48032f53827e84ec7c8674f7501b4f3cc9df8f6b
Alen <alenbeljimini@gmail.com>
2023-12-20 21:43:40 +0530

    printing from 101 to 110

--- ques.c
+++ ques.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
 void main(){
     printf("Who are you");
+
+    printf("I am trying to add something new");
+    printf("I dont know whether it will work");
+    for(int i=0;i<10;i++)
+        printf(i+100);
 }
